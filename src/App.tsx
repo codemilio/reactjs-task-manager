03b12dfa596eb62b960d85a8335667890b20bdb7
Carlos Silva <carlosemilio.contato@gmail.com>
2024-07-04 23:16:27 -0300

    feat: draft of project design

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { TaskItem } from "./components/TaskItem";
+import { PlusCircle } from '@phosphor-icons/react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main className="flex flex-col w-screen h-screen bg-base-600">
+      <header className="w-screen h-52 bg-base-700 flex items-center">
+        <h1 className="font-bold text-4xl m-auto">
+          <span className="text-primary">Task</span>
+          <span className="text-primary-dark">Manager</span>
+        </h1>
+      </header>
+      <div className="w-2/3 flex mt-[calc(-0px-1.5rem)] mx-auto gap-x-4">
+        <input 
+          className="w-full h-14 rounded-lg border bg-base-500 border-base-700 block mx-auto p-4 text-base-100 placeholder-base-300 outline-none focus:border-secondary-dark" 
+          placeholder="Adicione uma nova tarefa"
+        />
+        <button className="flex items-center justify-center gap-2 h-14 bg-secondary-dark focus:outline-none hover:bg-secondary p-4 rounded-lg text-sm font-bold text-base-100 transition-colors delay-75">
+          Criar
+          <PlusCircle size={20} />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="w-2/3 mx-auto mt-16">
+        <div className="flex justify-between font-bold">
+          <span className="text-primary"> Tarefas criada </span>
+          <span className="text-secondary"> Concluídas </span>
+        </div>
+        <div className="flex flex-col mt-4">
+          <TaskItem id="teste" value="indo algum saldhsa asjhd kjhahsdjha kjhkjdhsj hdjshakjs hkjshdkjsdhahkjashdkhk h kjahsdjhakj"/>
+        </div>
+      </div>
+    </main>
   )
 }
 
-export default App
