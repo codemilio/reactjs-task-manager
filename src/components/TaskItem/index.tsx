ef52ce8ad995508536a92616e54de87d2da7df21
Carlos Silva <carlosemilio.contato@gmail.com>
2024-07-04 23:14:18 -0300

    feat: create Task Item component

---
+++ src/components/TaskItem/index.tsx
@@ -0,0 +1,27 @@
+import { Trash } from "@phosphor-icons/react"
+
+type Props = {
+  id: string 
+  isChecked?: boolean 
+  value: string 
+}
+
+export function TaskItem({ id, isChecked = false, value }: Props) {
+  return (
+    <div className="flex flex-row justify-between items-center bg-base-500 text-base-100 rounded-lg p-4 border border-base-400">
+      <div className="flex gap-x-2">
+        <input 
+          type="checkbox" 
+          checked={isChecked}
+          className="bg-red-300 block" 
+          value={value}
+          id={id}
+          />
+        <label htmlFor={id}> {value} </label>
+      </div>
+      <button className="text-base-300 bg-transparent outline-none hover:text-danger transition-colors delay-75">
+        <Trash className="end"/>
+      </button>
+    </div>
+  )
+}
