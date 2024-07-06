7ad2d33e89b6b729c8230920dceeb8ca8c20b562
Carlos Silva <carlosemilio.contato@gmail.com>
2024-07-05 23:57:41 -0300

    fix: project template

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,81 @@
 import { TaskItem } from "./components/TaskItem";
 import { PlusCircle } from '@phosphor-icons/react'
+import { TextField } from "./components/TextField";
+import { Badge } from "./components/Badge";
 
 export function App() {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    const newValue = value.replace(/\D/g, '')
+    const limitedValue = newValue.slice(0, 2)
+    e.target.value = limitedValue
+  }
+
+  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.currentTarget.select()
+  }
+  
   return (
     <main className="flex flex-col w-screen h-screen bg-base-600">
       <header className="w-screen h-52 bg-base-700 flex items-center">
         <h1 className="font-bold text-4xl m-auto">
-          <span className="text-primary">Task</span>
-          <span className="text-primary-dark">Manager</span>
+          <TextField 
+            className="bg-transparent text-primary-dark w-11 focus:outline-none overflow-y-hidden" 
+            type="number" max={24} min={0} 
+            maxLength={2}
+            placeholder="00"
+            defaultValue="00"
+            onChange={handleInputChange}
+            onClick={handleClick}
+          />
+          <span className="text-primary">:</span>
+          <TextField 
+            className="bg-transparent text-primary-dark w-11 focus:outline-none"
+            type="number" max={60} min={0}
+            maxLength={2}
+            placeholder="00"
+            defaultValue="00"
+            onChange={handleInputChange}
+            onClick={handleClick}
+          />
+          <span className="text-primary">:</span>
+          <TextField 
+            className="bg-transparent text-primary-dark w-16 focus:outline-none"
+            type="number" max={60} min={0}
+            maxLength={2}
+            placeholder="ss"
+            defaultValue="00"
+            onChange={handleInputChange}
+            onClick={handleClick}
+            readOnly
+          />
         </h1>
       </header>
-      <div className="w-2/3 flex mt-[calc(-0px-1.5rem)] mx-auto gap-x-4">
-        <input 
-          className="w-full h-14 rounded-lg border bg-base-500 border-base-700 block mx-auto p-4 text-base-100 placeholder-base-300 outline-none focus:border-secondary-dark" 
-          placeholder="Adicione uma nova tarefa"
-        />
-        <button className="flex items-center justify-center gap-2 h-14 bg-secondary-dark focus:outline-none hover:bg-secondary p-4 rounded-lg text-sm font-bold text-base-100 transition-colors delay-75">
+      <div className="w-2/3 h-14 flex mt-[calc(-0px-1.75rem)] mx-auto gap-x-4">
+        <TextField placeholder="Adicione uma nova tarefa..."/>
+        <button className="flex h-full items-center justify-center gap-2 bg-secondary-dark focus:outline-none hover:bg-secondary p-4 rounded-lg text-sm font-bold text-base-100 transition-colors delay-75">
           Criar
           <PlusCircle size={20} />
         </button>
       </div>
 
-      <div className="w-2/3 mx-auto mt-16">
+      <div className="flex flex-col w-2/3 mx-auto gap-y-4 mt-14">
         <div className="flex justify-between font-bold">
-          <span className="text-primary"> Tarefas criada </span>
-          <span className="text-secondary"> Concluídas </span>
+          <span className="flex gap-x-2 text-primary"> 
+            Tarefas criadas 
+            <Badge value="5"/>
+          </span>
+          <span className="flex gap-x-2 text-secondary"> 
+            Concluídas 
+            <Badge value="0 de 5"/>
+          </span>
         </div>
-        <div className="flex flex-col mt-4">
-          <TaskItem id="teste" value="indo algum saldhsa asjhd kjhahsdjha kjhkjdhsj hdjshakjs hkjshdkjsdhahkjashdkhk h kjahsdjhakj"/>
+        <div className="flex flex-col">
+          <TaskItem id="teste" value="indo al"/>
+        </div>
+        <div className="flex justify-between font-bold">
+          <span className="text-primary"> Tempo estimado </span>
+          <span className="text-secondary"> Percorrido </span>
         </div>
       </div>
     </main>
